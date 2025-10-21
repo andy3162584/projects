48dfdb1e98a84e6fe64c190b8175a8cc16379129
andy3162584 <andy050629@gmail.com>
2025-10-21 23:21:04 +0800

    Initial commit

---
+++ sample1.java
@@ -0,0 +1,8 @@
+package com.pcsetting.example;
+
+public class Sample1{
+  public static void main(String[] args){
+	System.out.print("歡迎使用java!!");
+	System.out.print("開始使用java吧");
+  }
+}
---
+++ sample2.java
@@ -0,0 +1,8 @@
+package com.pcsetting.example;
+
+public class Sample2{
+	public static void main(String[] args){
+		System.out.println("歡迎使用java!!");
+		System.out.println("開始使用java吧");
+  }
+}
