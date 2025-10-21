71faaff8f32c12808b66101f6b05633484606b91
andy3162584 <andy050629@gmail.com>
2025-10-21 23:32:31 +0800

    Initial commit

---
+++ Sample3.java
@@ -0,0 +1,9 @@
+package com.pcsetting.example;
+
+public class Sample3{
+	public static void main(String[] args){
+		System.out.println("A");
+		System.out.println("歡迎使用java!!");
+		System.out.println(123);
+  }
+}
---
+++ Sample5.java
@@ -0,0 +1,8 @@
+package com.pcsetting.example;
+
+public class Sample5{
+	public static void main(String[] args){
+		System.out.println("八進位樹101的字元是\101");
+		System.out.println("十六進位樹0061的字元是\u0061");
+  }
+}
